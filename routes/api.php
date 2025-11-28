53e57034c63beb958e077fdc43f0ef9a052feef8
HerickMarra <herick.marra1@gmail.com>
2025-11-28 15:15:00 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::post('/cpf', function (Request $request) {
+
+    $entrada = $request->cpf . ",";
+    $i = 0;
+    $tmp = ""; 
+    $cpfs = [];
+
+    while (isset($entrada[$i])) {
+
+        if ($entrada[$i] == ',') {
+
+            $num = "";
+            $j = 0;
+            while (isset($tmp[$j])) {
+                if ($tmp[$j] >= "0" && $tmp[$j] <= "9") {
+                    $num .= $tmp[$j];
+                }
+                $j++;
+            }
+
+            $tamanho = 0;
+            $j = 0;
+            while (isset($num[$j])) { $tamanho++; $j++; }
+
+            if ($tamanho < 11) {
+                $faltam = 11 - $tamanho;
+
+                while ($faltam > 0) {
+                    $num = "0" . $num;
+                    $faltam--;
+                }
+            }
+
+            $mask = "";
+            $j = 0;
+            while (isset($num[$j])) {
+                if ($j == 3 || $j == 6) {
+                    $mask .= "." . $num[$j];
+                } else if ($j == 9) {
+                    $mask .= "-" . $num[$j];
+                } else {
+                    $mask .= $num[$j];
+                }
+                $j++;
+            }
+
+            array_push($cpfs, $mask);
+
+            $tmp = "";
+            $i++;
+            continue;
+        }
+
+        $tmp .= $entrada[$i];
+        $i++;
+    }
+
+    return response()->json($cpfs);
+});
